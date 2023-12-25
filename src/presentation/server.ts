f4a1938a4f087ff4b31dffb361a64800ba84d923
JaViKo500 <javikogutierrez64@gmail.com>
2023-12-25 02:14:20 -0500

    service cron

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,5 @@
+import { CronService } from './cron/cron-service';
+
 export class Server {
    constructor() {
    }
@@ -5,5 +7,13 @@ export class Server {
    public static start(){
       console.log('<--------------- JK Server --------------->');
       console.log('Server started');
+      CronService.createJob( 
+         '*/5 * * * * *',  
+         () => {
+            const date = new Date();
+            console.log('<--------------- JK Server --------------->');
+            console.log('5 seconds', date);
+         }
+      );
    }
 }
