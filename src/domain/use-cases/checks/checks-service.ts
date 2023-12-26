9b590a585acf4c29c8cf9f56a9d3ad7f5c359bb3
JaViKo500 <javikogutierrez64@gmail.com>
2023-12-26 06:15:41 -0500

    check implement log repository

--- src/domain/use-cases/checks/checks-service.ts
+++ src/domain/use-cases/checks/checks-service.ts
@@ -1,3 +1,5 @@
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repository";
 
 export interface CheckServiceUseCase {
    execute( url: string  ): Promise<boolean>;
@@ -7,6 +9,7 @@ type SuccessCallback = () => void;
 type ErrorCallback = ( error: string ) => void;
 export class CheckService implements CheckServiceUseCase {
    constructor(
+      private readonly logRepository: LogRepository,
       private readonly successCallback: SuccessCallback,
       private readonly errorCallback: ErrorCallback,
    ) {
@@ -18,10 +21,15 @@ export class CheckService implements CheckServiceUseCase {
          if ( !req.ok ) {
             throw new Error(`Error on check service ${ url }` );
          }
+         const log = new LogEntity( LogSeverityLevel.low, `Service ${url}` );
+         await this.logRepository.saveLog( log );
          this.successCallback();
          return true;
       } catch (error) {
-         this.errorCallback( `${error}` );
+         const errorMessage = `${error}`
+         const log = new LogEntity( LogSeverityLevel.high, errorMessage );
+         await this.logRepository.saveLog( log );
+         this.errorCallback( errorMessage );
          return false;
       }
    }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
 import { CheckService } from '../domain/use-cases/checks/checks-service';
+import { FileSystemDataSource } from '../infrastructure/datasources/file-system.datasource';
+import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository.impl';
 import { CronService } from './cron/cron-service';
+
+const fileSystemLogRepository =  new LogRepositoryImpl(
+   new FileSystemDataSource
+);
 
 export class Server {
    constructor() {
@@ -13,6 +19,7 @@ export class Server {
          () => {
             const url = 'https://google.com';
             new CheckService(
+               fileSystemLogRepository,
                () => console.log(`success url: ${url}`),
                (error) => console.log(error),
             ).execute( url );
