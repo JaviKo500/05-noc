7132369be5b31fe23ce8aa0dad567943f7526124
JaViKo500 <javikogutierrez64@gmail.com>
2023-12-26 06:04:51 -0500

    implement log repository

---
+++ src/infrastructure/repositories/log.repository.impl.ts
@@ -0,0 +1,17 @@
+import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
+import { LogRepository } from "../../domain/repository/log.repository";
+import { LogDataSource } from '../../domain/datasources/log.datasource';
+
+export class LogRepositoryImpl implements LogRepository {
+   constructor( 
+      private readonly logDatasource: LogDataSource
+   ) {
+      
+   }
+   async saveLog(log: LogEntity): Promise<void> {
+      await this.logDatasource.saveLog(log);
+   }
+   getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+      return this.logDatasource.getLogs(severityLevel);
+   }
+}
