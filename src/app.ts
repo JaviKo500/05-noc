d613bafc59fa9b09ecfa2705a8800a99ac98601d
JaViKo500 <javikogutierrez64@gmail.com>
2024-01-21 19:04:14 -0500

    connect mongo db

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import { envs } from './config/plugins/envs.plugins';
+import { MongoDataBase } from './data/mongo';
 import { Server } from './presentation/server';
 
 (async () => {
    await main();
 })();
 async function main() {
+   await MongoDataBase.connect({
+      mongoUrl: envs.MONGO_URL,
+      dbName: envs.MONGO_URL,
+   })
    await Server.start();
    // console.log('<--------------- JK App --------------->');
    // console.log(envs);
